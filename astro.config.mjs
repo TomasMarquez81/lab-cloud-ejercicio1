a53a12470ca59f535ee122f457838019b348e7c6
Tomás Márquez <johnniew_@hotmail.com>
2026-03-12 23:12:24 +0100

    Update files config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://TomasMarquez81.github.io', 
-  base: '/lab-cloud-ejercicio1', 
+  build:{
+    assets: 'css'
+  }
 });
