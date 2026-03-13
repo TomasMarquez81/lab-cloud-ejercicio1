e4fa482fd707926b699271161bdc01017a91aff1
Tomás Márquez <johnniew_@hotmail.com>
2026-03-14 00:09:13 +0100

    feat: Configure Vite base path to './' in Astro and a new Vite configuration file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  build:{
-    assets: 'css'
-  }
+    vite: {
+        base: './',
+    },
+    build: {
+        assets: 'css'
+    }
 });
